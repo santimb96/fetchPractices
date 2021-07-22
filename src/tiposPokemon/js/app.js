c117f7920d63be27d6cb0a6d150b4f71b0d67cee
Santiago <Martnez>
2021-07-22 14:46:24 +0200

    tabla de tipos Pokémon simple sin estilos

---
+++ src/tiposPokemon/js/app.js
@@ -0,0 +1,41 @@
+const APP = {
+    /**
+     * desde el mismo js se inicializa la app
+     */
+    inicializarDatos: function () {
+        const BOTON = document.getElementById("boton");
+        //se obtiene el botón y hasta que no es accionado, no se ejecuta el código
+        BOTON.addEventListener('click', function () {
+            //se obtiene el elemento propio y el elemento a comparar
+            let tuElemento = document.getElementById("tuElemento").value.toLowerCase();
+            let elementoComparar = document.getElementById("elementoComparar").value.toLowerCase();
+            //se pasan a la función que comprobará si es o no débil
+            this.comparar(tuElemento, elementoComparar);
+        }.bind(this));
+    },
+    /**
+     * @param tuElemento es el elemento propio
+     * @param elementoComparar es elemento a comparar
+     */
+    comparar: function (tuElemento, elementoComparar) {
+        const mostrarDebilidad = document.getElementById("mostrarDebilidad");
+        //si no es débil, se mostrará este mensaje hasta que se vuelva a proceder una comparación
+        mostrarDebilidad.innerHTML = `${tuElemento} NO es débil frente a ${elementoComparar}`;
+        //se importa el js externo y con la función module.LoQueHayaEnELJS, obtendremos sus propiedades
+        import ("./data.js").then(module => {
+            for (let i = 0; i < module.TIPOS.length; i++) {
+                //si el elemento existe, entra en el if
+                if (module.TIPOS[i].tipo === tuElemento) {
+                    for (let j = 0; j < module.TIPOS[i].debilidad.length; j++) {
+                        //si en el elemento existe la debilidad con la que se compara, entra en el if
+                        if (module.TIPOS[i].debilidad[j] === elementoComparar) {
+                            mostrarDebilidad.innerHTML = `${tuElemento} es débil frente a ${elementoComparar}`;
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
+APP.inicializarDatos();
---
+++ src/tiposPokemon/js/data.js
@@ -0,0 +1,55 @@
+export const TIPOS = [{
+    tipo: 'fuego',
+    debilidad: ['agua', 'roca', 'tierra']
+},{
+    tipo: 'agua',
+    debilidad: ['planta', 'electrico']
+},{
+    tipo: 'planta',
+    debilidad: ['fuego', 'hielo', 'volador', 'bicho','veneno']
+},{
+    tipo: 'acero',
+    debilidad: ['lucha', 'fuego', 'tierra']
+},{
+    tipo: 'bicho',
+    debilidad: ['volador', 'fuego', 'roca']
+},{
+    tipo: 'dragon',
+    debilidad: ['hada', 'hielo', 'dragon']
+},{
+    tipo: 'electrico',
+    debilidad: ['tierra']
+},{
+    tipo: 'fantasma',
+    debilidad: ['fantasma', 'siniestro']
+},{
+    tipo: 'hada',
+    debilidad: ['acero', 'veneno']
+},{
+    tipo: 'hielo',
+    debilidad: ['lucha', 'acero', 'roca', 'fuego']
+},{
+    tipo: 'lucha',
+    debilidad: ['psiquico', 'volador', 'hielo']
+},{
+    tipo: 'normal',
+    debilidad: ['lucha']
+},{
+    tipo: 'psiquico',
+    debilidad: ['bicho', 'fantasma', 'siniestro']
+},{
+    tipo: 'roca',
+    debilidad: ['lucha', 'tierra', 'veneno', 'volador']
+},{
+    tipo: 'siniestro',
+    debilidad: ['lucha', 'hada', 'bicho']
+},{
+    tipo: 'tierra',
+    debilidad: ['agua', 'planta', 'hielo']
+},{
+    tipo: 'veneno',
+    debilidad: ['tierra', 'psiquico']
+},{
+    tipo: 'volador',
+    debilidad: ['roca', 'hielo', 'electrico']
+}];
